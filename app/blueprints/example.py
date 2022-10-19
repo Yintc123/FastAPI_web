54b6e1fa39f9e1c6fd0bb7a3ae3dfb1bc1710637
unknown <yiqazwsx123@gmail.com>
2022-10-19 20:05:43 +0800

    Update the index.html.

--- app/blueprints/example.py
+++ app/blueprints/example.py
@@ -1,6 +1,15 @@
 # from flask import Blueprint
+from typing import Optional
 from fastapi import *
 from app.database.db import Order, db_session
+from pydantic import BaseModel
+
+class Order_Model(BaseModel):
+    order_id: Optional[int]
+    name: str
+    product_name: str
+    amount: int
+    price: int
 
 # api_page = Blueprint('Api', __name__)
 
@@ -17,11 +26,14 @@ def getAdsData():
 # 新增一筆訂單
 url_add_order='/order/add'
 @api_page.post(url_add_order)
-def create_order():
-    order=Order(customer_name='Tom', customer_id=0)
-    db_session.add(order)
-    db_session.commit()
-    db_session.close()
+def create_order(order:Order_Model):
+
+    for i in order:
+        print(i)
+    # order=Order(customer_name='Tom', customer_id=0)
+    # db_session.add(order)
+    # db_session.commit()
+    # db_session.close()
 
     return 'welcome to post test'
 
