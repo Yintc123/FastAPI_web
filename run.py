68cc7c98666d0e2c76a27dceb338131fa5c96b5e
unknown <yiqazwsx123@gmail.com>
2022-10-20 10:39:15 +0800

    Add two templates, modify_order.html and orders.html, and two routers.

--- run.py
+++ run.py
@@ -9,7 +9,15 @@ def index(request:Request):
     context={"request":request}
     return templates.TemplateResponse("index.html", context)
 
+@app.get("/orders", response_class=HTMLResponse)
+def index(request:Request):
+    context={"request":request}
+    return templates.TemplateResponse("orders.html", context)
 
+@app.get("/order", response_class=HTMLResponse)
+def index(request:Request):
+    context={"request":request}
+    return templates.TemplateResponse("modify_order_page.html", context)
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0')
